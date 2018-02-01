79f9be0bed6f9cd5d8fe5e2c653f67d90c44c9ff
Beatriz <beatrizrezener@gmail.com>
2018-02-01 21:35:54 -0200

    Categories images

--- src/models/place.ts
+++ src/models/place.ts
@@ -2,72 +2,120 @@
 export const categories = [
     {
         name: 'Hospedagem',
-        icon: '',
+        icon: 'assets/imgs/hotel.png',
         places: [
             {
                 name: 'Days Inn - London Ontario',
                 address: '1100 Wellington Road South, N6E 1M2 London, Canada –(6 km from airport)',
-                picture: '',
-                isFavorite: false,
+                picture: 'assets/imgs/hotel_example_1.jpg',
+                isFavorite: true,
             },
             {
                 name: 'Quality Suites London',
                 address: '1120 Dearness Drive, N6E 1N9 London, Canada (3 km from airport)',
-                picture: '',
-                isFavorite: false,
+                picture: 'assets/imgs/hotel_example_2.jpg',
+                isFavorite: true,
             },
             {
                 name: 'Doubletree by Hilton London Opens in new window',
                 address: '300 King Street, N6B 1S2 London, Canada',
-                picture: '',
+                picture: 'assets/imgs/hotel_example_3.jpg',
                 isFavorite: false,
             },
         ]
     },
     {
         name: 'Academia',
-        icon: '',
+        icon: 'assets/imgs/gym.png',
         places: [
             {
-                name: '',
-                address: '',
-                picture: '',
-                isFavorite: false,
+                name: 'Days Inn - London Ontario',
+                address: '1100 Wellington Road South, N6E 1M2 London, Canada –(6 km from airport)',
+                picture: 'assets/imgs/hotel_example_1.jpg',
+                isFavorite: true,
             },
             {
-                name: '',
-                address: '',
-                picture: '',
-                isFavorite: false,
+                name: 'Quality Suites London',
+                address: '1120 Dearness Drive, N6E 1N9 London, Canada (3 km from airport)',
+                picture: 'assets/imgs/hotel_example_2.jpg',
+                isFavorite: true,
             },
             {
-                name: '',
-                address: '',
-                picture: '',
-                isFavorite: false,
+                name: 'Doubletree by Hilton London Opens in new window',
+                address: '300 King Street, N6B 1S2 London, Canada',
+                picture: 'assets/imgs/hotel_example_3.jpg',
+                isFavorite: true,
             },
         ]
     },
     {
         name: 'Piscina',
-        icon: '',
+        icon: 'assets/imgs/swim.png',
         places: [
             {
-                name: '',
-                address: '',
-                picture: '',
+                name: 'Days Inn - London Ontario',
+                address: '1100 Wellington Road South, N6E 1M2 London, Canada –(6 km from airport)',
+                picture: 'assets/imgs/hotel_example_1.jpg',
+                isFavorite: true,
+            },
+            {
+                name: 'Quality Suites London',
+                address: '1120 Dearness Drive, N6E 1N9 London, Canada (3 km from airport)',
+                picture: 'assets/imgs/hotel_example_2.jpg',
                 isFavorite: false,
             },
             {
-                name: '',
-                address: '',
-                picture: '',
-                isFavorite: false,
+                name: 'Doubletree by Hilton London Opens in new window',
+                address: '300 King Street, N6B 1S2 London, Canada',
+                picture: 'assets/imgs/hotel_example_3.jpg',
+                isFavorite: true,
+            },
+        ]
+    },
+    {
+        name: 'Hidromassagem',
+        icon: 'assets/imgs/hidro.png',
+        places: [
+            {
+                name: 'Days Inn - London Ontario',
+                address: '1100 Wellington Road South, N6E 1M2 London, Canada –(6 km from airport)',
+                picture: 'assets/imgs/hotel_example_1.jpg',
+                isFavorite: true,
             },
             {
-                name: '',
-                address: '',
-                picture: '',
+                name: 'Quality Suites London',
+                address: '1120 Dearness Drive, N6E 1N9 London, Canada (3 km from airport)',
+                picture: 'assets/imgs/hotel_example_2.jpg',
+                isFavorite: true,
+            },
+            {
+                name: 'Doubletree by Hilton London Opens in new window',
+                address: '300 King Street, N6B 1S2 London, Canada',
+                picture: 'assets/imgs/hotel_example_3.jpg',
+                isFavorite: true,
+            },
+        ]
+    },
+    {
+        name: 'Spa',
+        icon: 'assets/imgs/spa.png',
+        places: [
+            {
+                name: 'Days Inn - London Ontario',
+                address: '1100 Wellington Road South, N6E 1M2 London, Canada –(6 km from airport)',
+                picture: 'assets/imgs/hotel_example_1.jpg',
+                isFavorite: true,
+            },
+            {
+                name: 'Quality Suites London',
+                address: '1120 Dearness Drive, N6E 1N9 London, Canada (3 km from airport)',
+                picture: 'assets/imgs/hotel_example_2.jpg',
+                isFavorite: true,
+            },
+            {
+                name: 'Doubletree by Hilton London Opens in new window',
+                address: '300 King Street, N6B 1S2 London, Canada',
+                picture: 'assets/imgs/hotel_example_3.jpg',
                 isFavorite: false,
             },
         ]
