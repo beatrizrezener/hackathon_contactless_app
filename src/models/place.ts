9f40c84d5204359742994349f40307c8804f9872
Luis H. Moreno <luishmcmoreno@gmail.com>
2018-02-01 21:08:42 -0200

    categories

---
+++ src/models/place.ts
@@ -0,0 +1,120 @@
+
+export const categories = [
+    {
+        name: 'Hospedagem',
+        icon: '',
+        places: [
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: 'Academia',
+        icon: '',
+        places: [
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: 'Piscina',
+        icon: '',
+        places: [
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+            {
+                name: '',
+                address: '',
+                picture: '',
+                isFavorite: false,
+                services: [
+                    {
+                        
+                    }
+                ]
+            },
+        ]
+    },
+]
